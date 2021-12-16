c8c7949f14ee61c8adf7e468f9cceed6b96600ce
dremaux <ldremaux@btslivh.eu>
2021-12-16 14:31:09 +0100

    .

--- lib/RecaptchaBundle/RecaptchaCompilerPass.php
+++ lib/RecaptchaBundle/RecaptchaCompilerPass.php
@@ -6,15 +6,13 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class RecaptchaCompilerPass implements CompilerPassInterface {
     
-    public function process(ContainerBuilder $container){
+    public function process(ContainerBuilder $container)
+    {
         if($container->hasParameter('twig.form.resources')){
             $resources = $container->getParameter('twig.form.resources') ?: [];
             array_unshift($resources,'@Recaptcha/fields.html.twig');
             $container->setParameter('twig.form.resources', $resources);
         }
     }
-
 }
-
-
 ?>
--- lib/RecaptchaBundle/Type/RecaptchaSubmitType.php
+++ lib/RecaptchaBundle/Type/RecaptchaSubmitType.php
@@ -16,7 +16,7 @@ class RecaptchaSubmitType extends AbstractType {
     {
         return 'recaptcha_submit';
     }
-
+    
 }
 
 ?>
