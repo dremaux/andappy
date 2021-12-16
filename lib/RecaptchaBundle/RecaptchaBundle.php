ac13136fc176d164b956ef797aa4d5d4e87fca83
dremaux <ldremaux@btslivh.eu>
2021-12-16 11:02:38 +0100

    submit recaptcha = button

--- lib/RecaptchaBundle/RecaptchaBundle.php
+++ lib/RecaptchaBundle/RecaptchaBundle.php
@@ -1,9 +1,15 @@
 <?php
 namespace Grafikart\RecaptchaBundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class RecaptchaBundle extends Bundle {
+
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+    } 
 
 }
 
