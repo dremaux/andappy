7d4ef6615bc041be342940575f519a9bbeff852e
dremaux <ldremaux@btslivh.eu>
2021-12-21 09:13:06 +0100

    test

--- public/index.php
+++ public/index.php
@@ -8,4 +8,18 @@ return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
 
+if(isset($_POST['submitpost'])){
+    if(isset($_POST['g-recaptcha-response'])){
+        $recaptcha = new \ReCaptcha\ReCaptcha('6LfnmLYdAAAAAN8R-5HYQVoI8c9dlAZ2XEf84Oqf');
+        $resp = $recaptcha->verify($_POST['g-recaptcha-response']);
+        if ($resp->isSuccess()) {
+            // Verified!
+        } else {
+            $errors = $resp->getErrorCodes();
+        }
+    } else {
+        var_dump('Captcha non rempli');
+    }
+}
+
 ?>
