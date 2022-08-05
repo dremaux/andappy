04ab57754a115724af69dce305ece2394367dc1b
dremaux <ldremaux@btslivh.eu>
2022-08-05 15:11:25 +0200

    shopcontroller correction

--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -1,16 +1,27 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
 
 
-class ShopController
+class ShopController extends AbstractController
 {
 
-    public function index()
+    public function __construct(Environment $twig)
     {
-        return new Response('shop/home.html.twig');
+        $this->twig = $twig;
+    }
+
+    /**
+     * @Route("/shop", name="shop")
+     * @return Response
+     */
+    public function index(): Response 
+    {
+        return $this->render('pages/home.html.twig');
     }
 
 }
