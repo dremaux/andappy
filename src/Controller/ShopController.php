51d75dad4767617e215618a050f71a587a428303
dremaux <ldremaux@btslivh.eu>
2022-08-05 11:40:24 +0200

    .

--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -1,25 +1,25 @@
 <?php
+
 namespace App\Controller;
 
-use App\Repository\PropertyRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
 
-Class ShopController extends AbstractController
+Class ShopController
 {
+
+    /**
+     * @var Environment
+     */
+
     public function __construct(Environment $twig)
     {
         $this->twig = $twig;        
     }
-
-    /**
-     * @Route("/shop", name="shop.index")
-     */
-    public function index()
+    
+    public function index(): Response 
     {
-        return $this->twig->render('shop/index.html.twig');
+        return new Response($this->twig->render('shop/index.html.twig'));
     }
 
 }
