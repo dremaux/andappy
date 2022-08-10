a08180bc9a23cb769a69cfccfd6a04253a5bd352
dremaux <ldremaux@btslivh.eu>
2022-08-10 23:47:07 +0200

    petite correction "email type" => "texte type"

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,8 +5,8 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -18,7 +18,7 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username', EmailType::class, [
+            ->add('username', TextType::class, [
                 'attr' => [
                     'class' => 'form-control',
                 ]
