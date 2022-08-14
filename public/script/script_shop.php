e963f8276b3b606b1ccf43c6a8cdecdfc4078859
Julian-crypto-byte <jlaspalles@btslivh.eu>
2022-08-14 15:32:54 +0200

    redirection

--- public/script/script_shop.php
+++ public/script/script_shop.php
@@ -1,7 +1,9 @@
 <?php
 $answer = $_POST['circle'];  
 if ($answer == "ans1") {          
-    $_VAR="<img src='images\properties\cercle-style.png'";      
+    $_VAR="<img src='images\properties\cercle-style.png'"; 
+    header('Location: https://projet-epsi-andappy.herokuapp.com/');
+    exit();     
 }
 else {
     echo 'Incorrect';
