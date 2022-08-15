a48e2511936f17ea2df946d9d4a835318eb2ffd6
Julian-crypto-byte <jlaspalles@btslivh.eu>
2022-08-15 15:44:00 +0200

    oo

--- public/script/script_shop.php
+++ public/script/script_shop.php
@@ -1,8 +1,7 @@
 <?php
 $answer = $_POST['circle'];  
 if ($answer == "ans1") {          
-    $_VAR="<img src='\images\properties\cercle-style.png'/>"; 
-    echo $_VAR;
+    $var = "\images\properties\cercle-style.png";
 }
 else {
     echo 'Incorrect';
